8f0e1a2e86f2a1d14280e846ab75a01159d437d2
OnlyBeeMan <lucasmarques4c@gmail.com>
2024-12-30 23:10:03 +0000

    Magic Block+ Episode

--- src/main/java/net/lukas/tutorialmod/item/ModFoodComponents.java
+++ src/main/java/net/lukas/tutorialmod/item/ModFoodComponents.java
@@ -6,5 +6,5 @@ import net.minecraft.entity.effect.StatusEffects;
 
 public class ModFoodComponents {
     public static final FoodComponent CAULIFLOWER = new FoodComponent.Builder().nutrition(3).saturationModifier(0.4F)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST,200),0.15f).build();
+            .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE,400),0.45f).build();
 }
